46adfbe6ea709c3dfee1ce195564ff1479d20224
MiroslavSvec <33287498+MiroslavSvec@users.noreply.github.com>
2018-06-07 20:04:07 +0100

    Finished profiles for now

--- README.md
+++ README.md
@@ -1,38 +1,49 @@
-# riddle-game
+# Riddle Game
+
 Simple Python riddle game
 
 ## Credits
 
 ### Boostrap template
+
 https://blackrockdigital.github.io/startbootstrap-sb-admin/index.html
+
 ### Databases
-#### Mixed
+
+Mixed
+
 https://github.com/inuits/hubot-scripts/blob/master/riddles.json
-#### General
+
+General
+
 https://github.com/azeemigi/riddle-server/blob/master/modules/core/src/main/resources/riddles.json
 
-
 ## Working on profiles creation
+
 For profile using JSON:
 
-	create separate file for each of the profile to work only with small files
+create separate file for each of the profile to work only with small files
 
 Created separate all-profiles.txt:
-	
-	 file with all profile names for events
+file with all profile names for events
 
 ## Bugs and Testing
 
-### Check values of the profile
-### Check if profile already exist
+Check values of the profile
 
-### Fix bug when all-profiles.txt exist without data (redirect) 
-	---  Fixed with checking if the len(profiles) is > 0
+Check if profile already exist
+
+Fix bug when all-profiles.txt exist without data (redirect) --  Fixed with checking if the len(profiles) is > 0
+
+Fix "Wiev all messages" in window button (bad redirecting)
+
+Fix bug with injecting nav links (multiple click add to link) --- Fixed
+
+Fix Chat window working in profile page only
 
 
-### Fix "Wiev all messages" in window button (bad redirecting)
+## Chngelog
 
-### Fix bug with injecting nav links (multiple click add to link)
-	--- Fixed
-
-### Fix Chat window working in profile page only
+Scraped redirection to login page as I do not think that is good user experiences.
+If user enters user name wich already exist (as he does not have to know about it)
+will use JS to handle the form check and and then redirect via Python
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import json
 from datetime import datetime
+from random import shuffle 
 from flask import Flask, redirect, request, render_template
 
 app = Flask(__name__)
@@ -18,62 +19,82 @@ def write_to_json(filename, write_mode, data):
 
 """ Read data """
 
-def read_data(filename):
-	data = []
+def read_txt(filename):
 	with open(f'{filename}', "r") as file:
 		data = file.readlines()
 		return data
 
-""" Login page """
+
+def read_json(filename):
+	with open(f'{filename}', "r") as file:
+		data = json.load(file)
+		return data
+
+
+
+""" Create profile page """
+
 
 @app.route('/', methods=["GET", "POST"])
-def index():	
-	## Need to fix bug with all user using same profile
-	""" with open("data/profiles.txt", "r") as x:
-		profile = x.readlines()
-		if profile:
-			return redirect(f"/{profile[0]}" """
-	## Create Profiles and store them in .json and .txt
+def index():
+		## Create Profiles and store them in .json (as profile data) and .txt (append to profiles list)
 	if request.method == "POST":
 		profile_name = request.form["username"]
-		profile_created = datetime.now().strftime("%H:%M:%S")
-		profiles = {}
-		profiles[f'{profile_name}'] = []
-		profiles[f'{profile_name}'].append(
-                    {'name': f'{profile_name}', 
-					'created': f'{profile_created}', 
-					'score': 0,
-					'right_answers': 0,
-					'wrong_answers': 0
-					})
-		write_to_json(f"data/profiles/{profile_name}.json", "w", profiles)
-		write_to_txt("data/profiles/all-profiles.txt", "a", f"{profile_name}" + '\n')
-		## Redirect user to profile page
-		return redirect(profile_name)
+		with open('data/profiles/all-profiles.txt', 'r') as file:
+			data = file.readlines()
+			for profile in data:
+				if profile.strip('\n') == profile_name:
+					return render_template("index.html")
+			profile_created = datetime.now().strftime("%H:%M:%S")
+			profiles = {}
+			profiles[f'{profile_name}'] = []
+			profiles[f'{profile_name}'].append(
+                                          {'name': f'{profile_name}',
+                                             'created': f'{profile_created}',
+                                             'score': 0,
+                                             'right_answers': 0,
+                                             'wrong_answers': 0,
+                                             'skipped_questions': 0,
+                                             })
+			write_to_json(f"data/profiles/{profile_name}.json", "w", profiles)
+			write_to_txt("data/profiles/all-profiles.txt",
+					             "a", f"{profile_name}" + '\n')
+			## Redirect user to profile page
+			return redirect(profile_name)
 	## Render index.html by default
 	return render_template("index.html")
+
 
 """ Profile page """
 
 @app.route('/<user_name>')
 def profile_page(user_name):
 	## Get profile data
-	profiles = read_data("data/profiles/all-profiles.txt")
+	profiles_data = read_txt("data/profiles/all-profiles.txt")
 	## Check if there is more then one profile
-	if len(profiles) > 0:
+	if len(profiles_data) > 0:
 		return render_template("profile.html",
-                          		user_name=user_name, page_title=f"{user_name}" + " Profile", profiles=profiles)
+                         user_name=user_name, page_title=f"{user_name}" + " profile", profiles=profiles_data)
 	## Render default template
 	return render_template("profile.html",
-                        	user_name=user_name, page_title=f"{user_name}" + " Profile")
+                        user_name=user_name, page_title=f"{user_name}" + " profile")
+
+""" Login page """
+
+@app.route('/<user_name>/login')
+def login(user_name):
+	return render_template("login.html",
+                        user_name=user_name, page_title=f"{user_name}" + " profile	")
+
 
 """ Riddles Game """
 
 @app.route('/<user_name>/riddle')
 def riddle(user_name):
-	profiles = read_data("data/profiles/all-profiles.txt")
+	profiles_data = read_txt("data/profiles/all-profiles.txt")
 	return render_template("riddles.html", 
-							user_name=user_name, page_title="Riddle Game", profiles=profiles)
+                        user_name=user_name, page_title="Riddle Game", profiles=profiles_data)
+
 
 """ Chat in separate page """
 
@@ -87,7 +108,7 @@ if __name__ == '__main__':
             port=os.getenv('PORT'),
             debug=True)
 
-
+""" Create 404 page and 500 error """
 """ Categories for questions """
 """ Score """
 """ Limit wrong answers """
