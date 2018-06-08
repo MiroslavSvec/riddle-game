9e6624e31d27b861112cb27b9a845dac87b93f7b
MiroslavSvec <33287498+MiroslavSvec@users.noreply.github.com>
2018-06-08 14:55:06 +0100

    Aded riddle game settings

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def index():
                                              'skipped_questions': 0,
                                              })
 			write_to_json(f"data/profiles/{profile_name}.json", "w", profiles)
-			write_to_txt("data/profiles/all-profiles.txt",
+			write_to_txt(f"data/profiles/all-profiles.txt",
 					             "a", f"{profile_name}" + '\n')
 			## Redirect user to profile page
 			return redirect(profile_name)
@@ -70,7 +70,7 @@ def index():
 @app.route('/<user_name>')
 def profile_page(user_name):
 	## Get profile data
-	profiles_data = read_txt("data/profiles/all-profiles.txt")
+	profiles_data = read_txt(f"data/profiles/all-profiles.txt")
 	## Check if there is more then one profile
 	if len(profiles_data) > 0:
 		return render_template("profile.html",
@@ -87,13 +87,25 @@ def login(user_name):
                         user_name=user_name, page_title=f"{user_name}" + " profile	")
 
 
+""" Riddles Game Setting"""
+
+
+@app.route('/<user_name>/riddle-g-setting', methods=["GET", "POST"])
+def riddle(user_name):
+	profiles_data = read_txt(f"data/profiles/all-profiles.txt")
+	return render_template("riddle-g-setting.html",
+                        user_name=user_name, page_title="Riddle Game Setting", profiles=profiles_data)
+
+
 """ Riddles Game """
 
-@app.route('/<user_name>/riddle')
-def riddle(user_name):
-	profiles_data = read_txt("data/profiles/all-profiles.txt")
-	return render_template("riddles.html", 
+
+@app.route('/<user_name>/riddle-game', methods=["GET", "POST"])
+def funcname(user_name):
+	profiles_data = read_txt(f"data/profiles/all-profiles.txt")
+	return render_template("riddle-game.html",
                         user_name=user_name, page_title="Riddle Game", profiles=profiles_data)
+
 
 
 """ Chat in separate page """
